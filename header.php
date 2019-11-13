33a273bd492322207ffd161678e6a3f0b72d9b99
root <root@nevernothing.jp>
2019-11-13 21:47:53 +0900

    20191113

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <meta charset="<?php bloginfo('charset'); ?>" />
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
-<title><?php wp_title(); ?></title>
+<title><?php wp_title('｜', true, 'right'); ?><?php bloginfo('name'); ?></title>
 <meta name="viewport" content="width=device-width,initial-scale=1.0" />
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_directory_uri(); ?>/style.css?" />
