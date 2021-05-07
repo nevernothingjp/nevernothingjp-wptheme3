67f5b6fa577e4a73704ffd9fa83f457e06721c6c
nevernothingjp <phantom@nevernothing.jp>
2021-05-07 21:47:52 +0900

    for sp

--- functions.php
+++ functions.php
@@ -83,7 +83,6 @@ if (!function_exists('thema_setup')):
         )
       );
     }
-
     register_sidebar(
       array(
         'name' => 'Wiget Header Left SP',
@@ -166,13 +165,10 @@ if (!function_exists('thema_setup')):
       );
 
 endif;
-
 // Add Actions
 add_action('after_setup_theme', 'thema_setup');
-
 // Add Editor Style
 add_editor_style('editor-style.css');
-
 // HTML HEAD
 remove_action('wp_head','rest_output_link_wp_head');
 remove_action('wp_head','wp_oembed_add_discovery_links');
@@ -184,15 +180,12 @@ remove_action('wp_head','wlwmanifest_link');
 remove_action('wp_head','adjacent_posts_rel_link_wp_head');
 remove_action('wp_head','wp_generator');
 remove_action('wp_head','wp_shortlink_wp_head');
-
 function remove_recent_comments_style() {
   global $wp_widget_factory;
   remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
 }
 add_action('widgets_init', 'remove_recent_comments_style');
-
 add_filter('wp_feed_cache_transient_lifetime', create_function('$a', 'return 600;' ) );
-
 add_filter( 'wp_calculate_image_srcset_meta', '__return_null' );
 add_filter( 'wp_resource_hints', 'remove_dns_prefetch', 10, 2 );
 function remove_dns_prefetch( $hints, $relation_type ) {
@@ -201,6 +194,5 @@ function remove_dns_prefetch( $hints, $relation_type ) {
     }
     return $hints;
 }
-
 remove_action('wp_head', 'print_emoji_detection_script', 7);
 remove_action('wp_print_styles', 'print_emoji_styles');
